f3f5b6cc68f378e8ae4df779b7cb146d2cf83fbf
zafertokerli <110773380+tokerli@users.noreply.github.com>
2023-09-09 14:04:01 +0300

    Baslangic

---
+++ mapitaramak/proje.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{   int x,y,b;
+    int array[100][100];
+    cout<<"Siniri girin:";
+    cin>>b;
+    for (x=0;x<b+1;x++)
+    {
+        for (y=0;y<b+1;y++)
+        {
+            
+            
+            
+        }
+    }
+   cout<<array;
+    return 0;
+}
