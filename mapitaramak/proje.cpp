b55bdb204b7f607cd4c8a222cf0367468c92a616
tokerli <110773380+tokerli@users.noreply.github.com>
2023-09-09 15:33:19 +0300

    mapi tarama tamamlandı.

--- mapitaramak/proje.cpp
+++ mapitaramak/proje.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char const *argv[])
-{   int x,y,b;
+{
+    int x, y, b;
     int array[100][100];
-    cout<<"Siniri girin:";
-    cin>>b;
-    for (x=0;x<b+1;x++)
+    cout << "Siniri girin:";
+    cin >> b;
+    for (x = 0; x < b + 1; x = x + 2)
     {
-        for (y=0;y<b+1;y++)
+        for (y = 0; y < b + 1; y++)
         {
-            
-            
-            
+            if (y < b)
+            {
+                cout << "(" << x << "," << y << ")"
+                     << " ";
+            }
+            else
+            {
+                cout << "(" << x << "," << y << ")"
+                     << " ";
+                for (y; y > -1; y = y - 1)
+                {
+                    cout << "(" << x + 1 << "," << y << ")"
+                         << " ";
+                }
+                break;
+            }
         }
     }
-   cout<<array;
+
     return 0;
 }
