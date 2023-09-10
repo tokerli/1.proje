911985ed418700af7d950d098834158ca05a6c17
zafertokerli <110773380+tokerli@users.noreply.github.com>
2023-09-10 17:33:18 +0300

    mapi tarama'da sorunlar çözüldü.

--- mapitaramak/proje.cpp
+++ mapitaramak/proje.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int x, y, b;
-    int array[100][100];
+    int map[100][100] = {false};
     cout << "Siniri girin:";
     cin >> b;
     for (x = 0; x < b + 1; x = x + 2)
@@ -12,17 +12,29 @@ int main(int argc, char const *argv[])
         {
             if (y < b)
             {
-                cout << "(" << x << "," << y << ")"
-                     << " ";
+                if (map[x][y] == 0)
+                {
+                    map[x][y] = 1;
+                    cout << "(" << x << "," << y << ")"
+                         << " ";
+                }
             }
             else
             {
-                cout << "(" << x << "," << y << ")"
-                     << " ";
-                for (y; y > -1; y = y - 1)
+                if (y == b and map[x][y] == 0)
                 {
-                    cout << "(" << x + 1 << "," << y << ")"
+                    map[x][y] = 1;
+                    cout << "(" << x << "," << y << ")"
                          << " ";
+                    for (y; y > -1; y = y - 1)
+                    {
+                        if (map[x + 1][y] == 0 and x < b)
+                        {
+                            map[x + 1][y] = 1;
+                            cout << "(" << x + 1 << "," << y << ")"
+                                 << " ";
+                        }
+                    }
                 }
                 break;
             }
